2e8aa0a95c5df7aa0aa0998fa04829f142ce73e6
h184937 <184937@stud.hvl.no>
2026-01-29 15:31:38 +0100

    oppdatert rute

--- src/Rute.java
+++ src/Rute.java
@@ -5,4 +5,12 @@ public class Rute {
     public Rute(int nr) {
         this.nr = nr;
     }
+
+    public int getNr(){
+        return nr;
+    }
+
+    public void setNr(int nr){
+        this.nr = nr;
+    }
 }
