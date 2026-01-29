9ba2a6a535372093c60e3a48638b7e4eefb7892f
h184937 <184937@stud.hvl.no>
2026-01-29 14:49:54 +0100

    oppdatert terning

--- src/Terning.java
+++ src/Terning.java
@@ -1,16 +1,9 @@
-public class Terning {
-
-    private int verdi;
-
-    //Konstruktør for terning
-    public Terning() {
-        this.verdi = verdi;
-    }
+public  class Terning {
 
     //Triller terning
-    public int trill() {
+    public static int trill() {
+
         //Tilfeldig verdi fra 1 til 6
-        verdi = (int) (Math.random() * 6) + 1;
-        return verdi;
+        return (int) (Math.random() * 6) + 1;
     }
 }
