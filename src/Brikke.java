644e079165643a45f6317a16ff3a23fd3863eda7
184913 <joans.askvikb@gmail.com>
2026-01-29 14:22:50 +0100

    Implementert Rute og brikke

--- src/Brikke.java
+++ src/Brikke.java
@@ -1,2 +1,17 @@
 public class Brikke {
+
+    private String navn;
+    private Rute rute;
+
+    public Rute getRute() {
+        return rute;
+    }
+
+    public void setRute(Rute rute) {
+        this.rute = rute;
+    }
+
+    public String getNavn() {
+        return navn;
+    }
 }
