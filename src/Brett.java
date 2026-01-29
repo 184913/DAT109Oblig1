6eb31d2b6884c030bb38c31712527b456a804ea2
184918 <184918@stud.hvl.no>
2026-01-29 15:07:46 +0100

    init commit

--- src/Brett.java
+++ src/Brett.java
@@ -1,8 +1,12 @@
 public class Brett {
 
+    //Liste med alle rutene våres
     private List<Rute> r;
+
+    //Liste av alle brikkene
     private List <Brikke > b;
 
+    //Metoden som finnerRuten brikken står på
     private Rute finnRute(){
         int pos = Brikke.getPosisjon();
 
