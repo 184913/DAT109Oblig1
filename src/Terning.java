c9a2acbee48a0812aebd17dd104ed486e08155f0
h184937 <184937@stud.hvl.no>
2026-01-29 13:48:10 +0100

    Klasse for terning

--- src/Terning.java
+++ src/Terning.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class Terning {
+
+    private int verdi;
+
+    //Triller terning
+    public int trill() {
+        verdi = (int) (Math.random() * 6) - 1); //Tilfeldig verdi fra 1 til 6
+        return verdi;
+    }
 }
