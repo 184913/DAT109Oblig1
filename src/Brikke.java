2dd047ea118626018594ce01c409a4ceaf8dbea8
h184937 <184937@stud.hvl.no>
2026-01-29 15:25:19 +0100

    implementasjon av Brikke

--- src/Brikke.java
+++ src/Brikke.java
@@ -1,2 +1,19 @@
 public class Brikke {
+
+    private String navn;
+    private Rute rute;
+
+    public Brikke(String navn, Rute rute){
+        this.navn = navn;
+        this.rute = rute;
+    }
+
+    public Rute getRute(){
+        return rute;
+    }
+
+    public void setRute(Rute rute){
+        this.rute = rute;
+    }
+
 }
