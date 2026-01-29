38e52d6b9be0feb75da51e900ab91925dccfcb96
h184937 <184937@stud.hvl.no>
2026-01-29 14:42:42 +0100

    implementasjon av terning

--- src/Terning.java
+++ src/Terning.java
@@ -2,12 +2,15 @@ public class Terning {
 
     private int verdi;
 
+    //Konstruktør for terning
+    public Terning() {
+        verdi = 0;
+    }
+
     //Triller terning
     public int trill() {
         //Tilfeldig verdi fra 1 til 6
-        verdi = (int) (Math.random() * 6) - 1;
+        verdi = (int) (Math.random() * 6) + 1;
         return verdi;
     }
-
-
 }
