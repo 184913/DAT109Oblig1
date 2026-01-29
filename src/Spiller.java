f602f8152df8adb328bbf6c28129c75a81e320f2
h184937 <184937@stud.hvl.no>
2026-01-29 14:22:38 +0100

    implementasjon av spiller

--- src/Spiller.java
+++ src/Spiller.java
@@ -1,2 +1,26 @@
 public class Spiller {
+
+    private String navn;
+    private Brikke brikke;
+
+    //Konstruktør for spiller
+    public Spiller(String navn, Brikke brikke) {
+        this.navn = navn;
+        this.brikke = brikke;
+    }
+
+
+    public void spillTrekk() {
+
+        //Triller terning
+        int sum = Terning.trill();
+
+        //finner den nye ruten
+        nyRute = flytt(brikke.getRute(), sum);
+
+        //Oppdater brikken med ny rute
+        brikke.setRute(nyRute);
+
+    }
+
 }
