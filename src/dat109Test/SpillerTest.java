f9e846918d37f7410f0873204ee25a018748d2be
h184937 <184937@stud.hvl.no>
2026-02-05 13:28:43 +0100

    oprette test

---
+++ src/dat109Test/SpillerTest.java
@@ -0,0 +1,26 @@
+package dat109Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpillerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getRute() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void spillTrekk() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getNavn() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void skrivUt() {
+    }
+}
---
+++ src/dat109Test/StigespillTest.java
@@ -0,0 +1,18 @@
+package dat109Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StigespillTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void spill() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void spillRunde() {
+    }
+}
